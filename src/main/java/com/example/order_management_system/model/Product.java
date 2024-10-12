a37ecab22d100ffad9702bcd6f1d6a5fdfef4dc3
Roman Sergeev <roman.sergeev1993@gmail.com>
2024-10-12 19:27:14 +0300

    PROJ-33 наработки

---
+++ src/main/java/com/example/order_management_system/model/Product.java
@@ -0,0 +1,35 @@
+package com.example.order_management_system.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "product")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "item_name")
+    private String itemName;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "price")
+    private BigDecimal price;
+
+    @Column(name = "quantity")
+    private int quantity;
+}
---
+++ src/main/java/com/example/order_management_system/service/ProductService.java
@@ -0,0 +1,46 @@
+package com.example.order_management_system.service;
+
+import com.example.order_management_system.model.Product;
+import com.example.order_management_system.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    private ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public Product findById(int id) {
+        return productRepository.findById(id).orElse(null);
+    }
+
+    @Transactional
+    public void save(Product product) {
+        productRepository.save(product);
+    }
+
+    @Transactional
+    public void update(int id, Product updateProduct) {
+        updateProduct.setId(id);
+        productRepository.save(updateProduct);
+    }
+
+    @Transactional
+    public void removeById(int id) {
+        productRepository.deleteById(id);
+    }
+}
