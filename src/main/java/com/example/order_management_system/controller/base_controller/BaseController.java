5198c9f020ae453086fa6b6e9f53607387406326
Roman Sergeev <roman.sergeev1993@gmail.com>
2024-11-12 01:13:08 +0300

    PROJ-34 workings 5

---
+++ src/main/java/com/example/order_management_system/controller/base_controller/BaseController.java
@@ -0,0 +1,8 @@
+package com.example.order_management_system.controller.base_controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping(value = "/api/v1/store/")
+public abstract class BaseController {
+
+}
