cb422cb72da39def05a377e85a7de63fabb6113f
Roman Sergeev <roman.sergeev1993@gmail.com>
2024-11-12 17:20:15 +0300

    PROJ-34 workings 19

--- src/main/java/com/example/order_management_system/dto/registration/request/RegistrationRequest.java
+++ src/main/java/com/example/order_management_system/dto/registration/request/RegistrationRequest.java
@@ -4,10 +4,14 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class RegistrationRequest {
 
     @Length(min = 1, max = 30)
