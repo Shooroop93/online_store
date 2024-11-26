c23e0d819f2103dc54df0045938fe4013288641d
Roman Sergeev <roman.sergeev1993@gmail.com>
2024-11-26 23:36:35 +0300

    PROJ-41 added two new project

---
+++ delivery/src/main/java/com/example/delivery/controller/DefaultController.java
@@ -0,0 +1,25 @@
+package com.example.delivery.controller;
+
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@ConfigurationProperties(prefix = "application.notices")
+@Setter
+@RequestMapping(value = "/api/v1/delivery/")
+public class DefaultController {
+
+    private String health;
+
+    @GetMapping("/health")
+    public ResponseEntity<String> health() {
+        return ResponseEntity.status(HttpStatus.OK).body(health);
+    }
+}
